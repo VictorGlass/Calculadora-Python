31973d2a38f3f057a2064c17761c41affb889b71
VictorGlass <enginemaster551@gmail.com>
2024-09-11 13:27:25 -0300

    Subiendo el codigo

---
+++ calculadora.py
@@ -0,0 +1,77 @@
+#Creacion de una Calculadora usando Python.
+
+class Calculador:
+    def __init__(self):
+        self.num1 = 0
+        self.num2 = 0
+        self.operacion = ''
+    
+    def sumar(self):
+        print("-------------------------------------")
+        self.num1 = int(input("Ingresa el primer numero: "))
+        print("-------------------------------------")
+        self.num2 = int(input("Ingresa el segundo numero: "))
+        self.operacion = '+'
+        self.calcular()
+    
+    def restar(self):
+        print("-------------------------------------")
+        self.num1 = int(input("Ingresa el primero numero: "))
+        print("-------------------------------------")
+        self.num2 = int(input("Ingresa el segundo numero: "))
+        self.operacion = '-'
+        self.calcular()
+    
+    def multiplicar(self):
+        print("-------------------------------------")
+        self.num1 = int(input("Ingresa el primer numero: "))
+        print("-------------------------------------")
+        self.num2 = int(input("Ingresa el segundo numero: "))
+        self.operacion = '*'
+        self.calcular()
+    
+    def dividir(self):
+        print("-------------------------------------")
+        self.num1 = int(input("Ingresa el primer numero: "))
+        print("-------------------------------------")
+        self.num2 = int(input("Ingresa el segundo numero: "))
+        self.operacion = '/'
+        self.calcular()
+    
+    def calcular(self):
+        if self.operacion == '+':
+            self.resultado = self.num1 + self.num2
+        elif self.operacion == '-':
+            self.operacion = self.num1 - self.num2
+        elif self.operacion == '*':
+            self.operacion = self.num1 * self.num2
+        elif self.operacion == '/':
+            self.operacion = self.num1 / self.num2
+        else:
+            self.resultado = 0
+        print("-------------------------------------")
+        print(f"El resultado de la operacion es: {self.resultado}")
+        print("-------------------------------------")
+
+calc = Calculador()
+
+while True:
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicacion")
+    print("4. Division")
+    print("===================")
+
+    opcion = int(input("Ingresa una opcion: "))
+    if opcion == 1:
+        calc.sumar()
+    elif opcion == 2:
+        calc.restar()
+    elif opcion == 3:
+        calc.multiplicar()
+    elif opcion == 4:
+        calc.dividir()
+    else:
+        print("Opcion incorrecta.")
+
+    
